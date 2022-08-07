541e545ba7888103fe40cf42249fc8fe47a52531
akshat-sonic <akshat.soni@navi.com>
2022-08-07 11:33:53 +0530

    adds jwt config and password encoder

---
+++ src/main/java/com/bootcamp/bookshop/auth/JwtAuthConfig.java
@@ -0,0 +1,23 @@
+package com.bootcamp.bookshop.auth;
+
+import com.bootcamp.bookshop.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class JwtAuthConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService);
+    }
+}
